7c22b91a2b857706f48fa20f3dc19dddb5aa3a47
Cara Noyes <cnnoyes@outlook.com>
2021-06-16 13:38:36 -0500

    validation completed, started on updates to launch list

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.addEventListener("formSubmit", function(event) {
 
 window.addEventListener("load", function() {
    let form = document.querySelector("form");
-   console.log('form', form);
+   // console.log('form', form);
    form.addEventListener("submit", function(event) {
       let pilotName = document.querySelector("input[name = pilotName");
       let copilotName = document.querySelector("input[name = copilotName");
@@ -13,17 +13,30 @@ window.addEventListener("load", function() {
       let cargoMass = document.querySelector("input[name = cargoMass");
       if (isNaN(pilotName.value) === false || isNaN(copilotName.value) === false) {
          alert("Pilot and Copilot names must made up of a string of letters.");
-      } else if (isNaN(fuelLevel.value) === true || isNaN(cargoMass.value === true)) {
+      } else if (isNaN(fuelLevel.value) === true || isNaN(cargoMass.value) === true) {
          alert("Fuel level and cargo mass must be numbers.");
       }
       else if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
          alert("All fields are required for submission.");
       } 
+      document.getElementById("pilotStatus").innerHTML = `Pilot, ${pilotName}, is ready`;
+      document.getElementById("copilotStatus").innerHTML = `Co-pilot, ${copilotName}, is ready`;
+      if (fuelLevel.value < 10000){
+         document.getElementById("fuelStatus").style.visibility = "visible";
+         document.getElementById("fuelStatus").innerHTML = "There is not enough fuel to launch.";
+         document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch";
+         document.getElementById("launchStatus").style.color = "red";
+      }
+   
    });
+
+   
    // pilotName.addEventListener("formSubmit", function(event) {
    //    alert("Must enter a pilot name.")
    // });
 });
+
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
